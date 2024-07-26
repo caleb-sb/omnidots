1ce46b021928ae08dc204e34f9113778c5eefc77
Caleb <caleb@email.com>
2024-07-26 11:33:07 +0200

    Add fish config

---
+++ config/fish/config.fish
@@ -0,0 +1,37 @@
+# Only execute this file once per shell.
+set -q __fish_home_manager_config_sourced; and exit
+set -g __fish_home_manager_config_sourced 1
+
+set fish_greeting
+
+status is-login; and begin
+
+    # Login shell initialisation
+
+
+end
+
+status is-interactive; and begin
+
+    # Abbreviations
+
+
+    # Aliases
+    alias l 'eza -lh --icons=auto'
+    alias ld 'eza -lhD --icons=auto'
+    alias lg lazygit
+    alias ll 'eza -lha --icons=auto --sort=name --group-directories-first'
+    alias ls 'eza -1'
+
+    # Interactive shell initialisation
+    if test "$TERM" != dumb
+        eval (/home/caleb/.nix-profile/bin/starship init fish)
+
+    end
+
+    if set -q KITTY_INSTALLATION_DIR
+        set --global KITTY_SHELL_INTEGRATION no-rc
+        source "$KITTY_INSTALLATION_DIR/shell-integration/fish/vendor_conf.d/kitty-shell-integration.fish"
+        set --prepend fish_complete_path "$KITTY_INSTALLATION_DIR/shell-integration/fish/vendor_completions.d"
+    end
+end
