fe37f3596d06f971b99b7e1662eb20b1130f0b5b
Caleb <caleb@email.com>
2024-07-26 12:20:29 +0200

    Add flathub remote and assume yes

--- system/flathub.sh
+++ system/flathub.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
-flatpak install flathub md.obsidian.Obsidian
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+
+flatpak install -y flathub md.obsidian.Obsidian
