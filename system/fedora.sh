d731f9f8bb27d14c380d7fa42a4edd11cf6c2150
Caleb <caleb@email.com>
2024-07-26 12:23:16 +0200

    Assume yes on chrome install

--- system/fedora.sh
+++ system/fedora.sh
@@ -4,6 +4,8 @@ sudo dnf install fedora-workstation-repositories
 
 sudo dnf config-manager --set-enabled google-chrome
 
-sudo dnf install snapd
+sudo dnf install -y snapd
 
-sudo dnf install flatpak
+sudo dnf install -y flatpak
+
+sudo dnf install -y google-chrome-stable
