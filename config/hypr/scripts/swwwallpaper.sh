a4d88cdaa810da3e5fd3e0d7f0677e0b0a81deab
Caleb <caleb@email.com>
2024-09-11 20:41:26 +0200

    Update according to current config
    - screenshot shortcut doesnt work
    - sharing xwayland has issues
    - might have lost some things

--- config/hypr/scripts/swwwallpaper.sh
+++ config/hypr/scripts/swwwallpaper.sh
@@ -3,23 +3,23 @@
 # define functions
 
 Wall_Set() {
-	if [ -z $xtrans ]; then
-		xtrans="grow"
-	fi
+  if [ -z $xtrans ]; then
+    xtrans="grow"
+  fi
 
-	swww img /home/caleb/.config/swww/Catppuccin-Mocha/escape_velocity.jpg \
-		--transition-bezier .43,1.19,1,.4 \
-		--transition-type "$xtrans" \
-		--transition-duration 0.7 \
-		--transition-fps 60 \
-		--invert-y \
-		--transition-pos "$(hyprctl cursorpos)"
+  swww img /home/caleb/.config/swww/Catppuccin-Mocha/Staircase.png \
+    --transition-bezier .43,1.19,1,.4 \
+    --transition-type "$xtrans" \
+    --transition-duration 0.7 \
+    --transition-fps 60 \
+    --invert-y \
+    --transition-pos "$(hyprctl cursorpos)"
 }
 
 # check swww daemon and set wall
 swww query
 if [ $? -eq 1 ]; then
-	swww init
+  swww init
 fi
 
 Wall_Set
