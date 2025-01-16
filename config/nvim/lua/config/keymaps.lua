074ec8abdd6d4c118d7f97bbf82b0e5498dc226f
Caleb Bredekamp <caleb_sb@icloud.com>
2025-01-16 19:35:19 +0200

    Update lazyvim

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -15,5 +15,5 @@ vim.keymap.set("n", "c", '"_c')
 -- Don't enter the void
 vim.keymap.set("n", "Q", "<nop>")
 -- Remap for dealing with word wrap
-vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
-vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+vim.keymap.set("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
